bf68424fd576b3aec420fb9bb6523a515e29f779
naquio <ignaciocanal@live.com.ar>
2024-07-18 20:33:24 -0300

    empieza a parecer algo

--- js/app.js
+++ js/app.js
@@ -1,18 +1,47 @@
 const productos = [
-  { id: 1, name: "Martillo", price: 20000, productPhoto: "", paymentMethods: ["Tarjeta de Crédito", "Efectivo"]},
-  { id: 2, name: "Destornillador", price: 8000, productPhoto: "", paymentMethods: ["Tarjeta de Crédito", "Efectivo"]},
-  { id: 3, name: "Taladro", price: 200000, productPhoto: "", paymentMethods: ["Tarjeta de Crédito", "Efectivo", "Tarjeta de Débito"]},
-  { id: 4, name: "Amoladora", price: 250000, productPhoto: "", paymentMethods: ["Tarjeta de Crédito", "Transferencia", "Pago con QR", "Efectivo", "Tarjeta de débito"]},
-  { id: 5, name: "Clavos y Tornillos", price: 100, productPhoto: "", paymentMethods: ["Efectivo", "Tarjeta de débito", "Pago con QR"]},
-  { id: 6, name: "Cinta métrica", price: 10000, productPhoto: "", paymentMethods: ["Efectivo", "Tarjeta de débito", "Pago con QR"]},
+  { id: '1', nombre: "Martillo", precio: 20000, metodosDePago: ["TC", " Efectivo"]},
+  { id: '2', nombre: "Destornillador", precio: 8000, metodosDePago: ["TC", " Efectivo"]},
+  { id: '3', nombre: "Taladro", precio: 200000, metodosDePago: ["TC", " Efectivo", " TD"]},
+  { id: '4', nombre: "Amoladora", precio: 250000, metodosDePago: ["TC", " QR", " Efectivo", " TD"]},
+  { id: '5', nombre: "Clavos y Tornillos", precio: 100, metodosDePago: ["Efectivo", " TD", " QR"]},
+  { id: '6', nombre: "Cinta métrica", precio: 10000, metodosDePago: ["Efectivo", " TD", " QR"]},
 ];
 
-let opcion = prompt("¿Qué quiere saber? Elija una opción: \n Productos \n salir")
+const locaciones = ["Buenos Aires 3200","Av Pellegrini 654","Italia esq. Maipú"];
 
-productos.forEach((e) => {
-  console.log(`El producto ${e.name} sale ${e.price} y se puede pagar con ${e.paymentMethods}`)
-})
+function bienvenido(){
+  let opcion = prompt("Bienvenidos a nuestros artículos de Ferretería, por favor elija una opción para continuar: \n Productos \n salir").toLocaleLowerCase();
+  if (opcion === "salir"){
+    alert('Gracias por visitarnos')
+    return
+  } else if (opcion === "productos"){
+    productos.forEach((e) => {
+      console.log(`El producto ${e.nombre} sale ${e.precio} y se puede pagar con ${e.metodosDePago}`)
+    })
+  } else {
+    alert(`ingrese una opción válida por favor.`);
+    bienvenido();
+  }
+}
+function elegirProductos(){
+  let elegir = parseInt(prompt(`Eliga el número de alguno de los productos para saber más: \n 1 - Martillo \n 2 - Destornillador \n 3 - Taladro \n 4 - Amoladora \n 5 - Clavos y Tornillos \n 6 - Cinta Métrica \n SALIR.`));
+  if (elegir === 1){
+    const martillo = productos.find(producto =>producto.id === 1);
+    console.log(martillo);
+//    alert(`El producto ${martillo.nombre}, posee un valor de $${martillo.precio}, las formas de pago son: ${martillo.metodosDePago}`);
+  }
+}
+
+  
+bienvenido();
+elegirProductos();
 
 //let ingreso = parseInt(prompt("ingrese el producto que quiere conocer: \n 1) Martillo \n 2) Destornillador \n 3) Taladro \n 4) Amoladora \n 5) Clavos y Tornillos \n 6) Cinta Métrica " ))
 //const buscar = productos.find(e => e.id === ingreso)
 //console.log(buscar);
+
+//productos.push({id: '7', nombre: "Copia de Llaves", precio: 2000, foto:"", metodosDePago: ["Efectivo", "Pago con QR","Tarjeta de débito"]})
+
+//productos.forEach((e) => {
+//  console.log(`El producto ${e.nombre} sale ${e.precio} y se puede pagar con ${e.metodosDePago}`)
+//})
